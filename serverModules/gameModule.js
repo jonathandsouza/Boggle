db90dc2b79f8dc629c635255803768eafd090752
jonathandsouza <mail2jona@yahoo.com>
2015-08-05 08:29:34 +0530

    gameModule File

---
+++ serverModules/gameModule.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+
+    'use strict';
+
+
+    function gameManager() {
+
+        this.challengeID = 0;
+        this.challenges = [];
+
+        this.createChallenge = function () {
+
+
+
+        }
+    }
+
+
+
+    return new gameManager();
+}
